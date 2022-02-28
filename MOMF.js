bc94df8f28506355d682f6978eb9cd8a9fe9a3f6
Mariyam_op <91929003+8899MariyamShaikh@users.noreply.github.com>
2022-02-28 15:51:55 +0530

    Add files via upload

---
+++ MOMF.js
@@ -0,0 +1,24 @@
+function Kwitter()
+{
+    window.location="indx.html";
+}
+
+function Paint_1()
+{
+    window.location="paint.html";
+}
+
+function Paint_2()
+{
+    window.location="paint2.html";
+}
+
+function Power_Rangers()
+{
+    window.location="PR.html"
+}
+
+function GTK()
+{
+    window.location="GTK.html"
+}
---
+++ main.js
@@ -0,0 +1,13 @@
+function ArtG() 
+{
+    window.location="ArtG.html";
+}
+ function VoiceS()
+ {
+     window.location="Take_Selfie.html";
+ }
+
+ function MOMF()
+ {
+     window.location="MOMF.html";
+ }
